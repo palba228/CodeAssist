37a8201f0e1dbe88f4d0813618bb3165415fa5f0
palba228 <daniilmasnikov111@gmail.com>
2026-06-19 17:46:12 +0300

    Delete ide-core/src/main/kotlin/dev/ide/core/network/AiNetworkClient.kt

--- ide-core/src/main/kotlin/dev/ide/core/network/AiNetworkClient.kt
+++
@@ -1,6 +0,0 @@
-package dev.ide.ui.network
-
-expect class AiNetworkClient() {
-    suspend fun sendCodeToAi(apiUrl: String, apiKey: String, provider: String, prompt: String, fileContent: String): String
-}
-
